5b3e6c39be7609de8ff6082dcc9226cbcab0d0dd
katman-Generation <tkatema5@gmail.com>
2023-10-29 06:59:58 +0200

    styling the home page

--- app.py
+++ app.py
@@ -5,34 +5,42 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def home():
-    return(render_template('index.html'))
+    """rendering the home page"""
+    return(render_template('layout.html'))
 
 @app.route("/about")
 def about():
+    """the url for about page"""
     return(render_template('about.html'))
 
 @app.route("/mission")
 def mission():
+    """the url for the mission page"""
     return(render_template('mission.html'))
 
 @app.route("/events")
 def events():
+    """the url for the events page"""
     return(render_template('events.html'))
 
 @app.route("/inspiration")
 def inspiration():
+    """the url for the inspiration page"""
     return(render_template('inspiration.html'))
 
 @app.route("/contacts")
 def contacts():
+    """the url for the contact page"""
     return(render_template('contacts.html'))
 
 @app.route("/board")
 def board():
+    """the url for the board page"""
     return(render_template('board.html'))
 
 @app.route("/Donations")
 def Donations():
+    """the url for the Donation function"""
     return(render_template('Donations.html'))
 
 if __name__ == "__main__":
