b490434fbe7f27d5908a8fb0ed423d2cb19c303e
katman-Generation <tkatema5@gmail.com>
2023-11-27 05:49:21 +0200

    making changes to the homepage

---
+++ static/index.js
@@ -0,0 +1,8 @@
+function openNavigation() {
+  document.getElementById("pmySidenav").style.width = "50%";
+}
+
+/* Close/hide the sidenav */
+function closeNavigation() {
+  document.getElementById("pmySidenav").style.width = "0";
+}
